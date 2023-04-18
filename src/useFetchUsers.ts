d85327bd4b13a9f570f563e2d3902ed4795bf899
Zaneta <zaneta.gorska1@gmail.com>
2023-04-18 13:20:59 +0200

    feat: reset results for new query

--- src/useFetchUsers.ts
+++ src/useFetchUsers.ts
@@ -12,6 +12,10 @@ export const useFetchUsers = (query: string, page: number) => {
   }
 
   useEffect(() => {
+    setUsers([])
+  }, [query])
+
+  useEffect(() => {
     // @TODO: Cover API error
     const fetchUsers = async () => {
       const res = await axios.get<UsersResponse>("https://api.github.com/search/users", {  
